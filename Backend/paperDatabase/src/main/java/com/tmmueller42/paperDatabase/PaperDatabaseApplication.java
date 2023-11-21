6919449dc1d4e6f13487f1564965ef0cb373da99
tm-mueller42 <123808636+tm-mueller42@users.noreply.github.com>
2023-11-21 14:28:32 +0100

    Add files via upload

---
+++ Backend/paperDatabase/src/main/java/com/tmmueller42/paperDatabase/PaperDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.tmmueller42.paperDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperDatabaseApplication.class, args);
+	}
+
+}
---
+++ Backend/paperDatabase/src/test/java/com/tmmueller42/paperDatabase/PaperDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmmueller42.paperDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
