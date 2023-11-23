acebbcab0aa4815f6fce8877d1548461c47ba520
tm-mueller42 <tm.mueller42@gmail.com>
2023-11-23 22:48:43 +0100

    add jwtGenerator and register + login endpoints.

---
+++ Backend/paperDatabase/src/main/java/com/tmmueller42/paperDatabase/api/endpoint/RegisterEndpoint.java
@@ -0,0 +1,30 @@
+package com.tmmueller42.paperDatabase.api.endpoint;
+
+import com.thisteam.logisticsprint2.persistence.entity.User;
+import com.thisteam.logisticsprint2.service.UserService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("register")
+public class RegisterEndpoint {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterEndpoint(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping
+    @Secured("ROLE_ADMIN")
+    User create(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userService.create(user);
+    }
+}
