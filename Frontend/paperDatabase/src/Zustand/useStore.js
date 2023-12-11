03d0be1612fce8510e1cdd9791f78f904a7600dc
tm-mueller42 <tm.mueller42@gmail.com>
2023-12-11 15:29:32 +0100

    Add Persist to Zustand/UseStore.

--- Frontend/paperDatabase/src/Zustand/useStore.js
+++ Frontend/paperDatabase/src/Zustand/useStore.js
@@ -1,16 +1,24 @@
 import {create} from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export const useStore = create((set) => ({
-  username: null,
-  authorities: [],
-  loggedIn: false,
-  jwt: null,
-  setUsername: (newUserName) => set({ username: newUserName }),
-  clearUsername: () => set({ username: null }),
-  setAuthorities: (newAuthorities) => set({authorities: newAuthorities}),
-  clearAuthorities: () => set({authorities: null}),
-  setLoggedInTrue: () => set({loggedIn: true}),
-  setLoggedInFalse: () => set({loggedIn: false}),
-  setJwt: (newJwt) => set({jwt:newJwt}),
-  clearJwt: () => set({jwt: null})
-}))
+export const useStore = create(
+   persist( (set) => (
+    {
+    username: null,
+    authorities: [],
+    loggedIn: false,
+    jwt: null,
+    setUsername: (newUserName) => set({ username: newUserName }),
+    clearUsername: () => set({ username: null }),
+    setAuthorities: (newAuthorities) => set({authorities: newAuthorities}),
+    clearAuthorities: () => set({authorities: null}),
+    setLoggedInTrue: () => set({loggedIn: true}),
+    setLoggedInFalse: () => set({loggedIn: false}),
+    setJwt: (newJwt) => set({jwt:newJwt}),
+    clearJwt: () => set({jwt: null})
+    }),
+    {
+      name: 'paper-storage',
+    }
+  )
+)
